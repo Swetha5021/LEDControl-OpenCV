a6949d99b58cc5d8ed1b3d5310e848fe386d89d2
Swetha Sivasankararaj <110710815+Swetha5021@users.noreply.github.com>
2024-03-25 12:08:23 +0530

    Update StandardFirmata.ino

--- StandardFirmata.ino
+++ StandardFirmata.ino
@@ -18,8 +18,6 @@
   License as published by the Free Software Foundation; either
   version 2.1 of the License, or (at your option) any later version.
 
-  See file LICENSE.txt for further informations on licensing terms.
-
 */
 
 #include <Servo.h>
